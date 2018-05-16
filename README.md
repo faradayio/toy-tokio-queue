eb4f11fea3b2bb4b355503b1011fd625facf6b47
Eric Kidd <git@randomhacks.net>
2018-05-16 14:02:41 -0400

    Paste in example server code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Toy Tokio queuing primitives
+
+This is a public "spike" to see if Tokio can be used to fix https://github.com/Antti/rust-amqp/issues/72 by implementing a full-duplex TLS connection.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+//! A tiny async echo server with Tokio. This code is taken directly from
+//! https://tokio.rs/
+
+extern crate tokio;
+
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // Bind the server's socket
+    let addr = "127.0.0.1:12345".parse().unwrap();
+    let tcp = TcpListener::bind(&addr).unwrap();
+
+    // Iterate incoming connections
+    let server = tcp.incoming().for_each(|tcp| {
+        // Split up the read and write halves
+        let (reader, writer) = tcp.split();
+
+        // Copy the data back to the client
+        let conn = io::copy(reader, writer)
+            // print what happened
+            .map(|(n, _, _)| {
+                println!("wrote {} bytes", n)
+            })
+            // Handle any errors
+            .map_err(|err| {
+                println!("IO error {:?}", err)
+            });
+
+        // Spawn the future as a concurrent task
+        tokio::spawn(conn);
+
+        Ok(())
+    })
+    .map_err(|err| {
+        println!("server error {:?}", err);
+    });
+
+    // Start the runtime and spin up the server
+    tokio::run(server);
 }
